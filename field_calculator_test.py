5d6179c7a21072cd657be4f2bc7e4882a711df71
yousefeleraky11 <yr.eraqy@wakeb.tech>
2026-01-27 11:51:49 +0200

    added geoserver

--- field_calculator_test.py
+++ field_calculator_test.py
@@ -9,16 +9,13 @@ import requests
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-class FieldCalculator:
-    def __init__(self,gdf,payload,target_field):
-        self.gdf=gdf
-        self.p=payload
-        self.target_field=target_field
+
 
 class CalcStrategy(ABC):
     """ every engine should implement this interface """
     @abstractmethod
     def execute(self, payload: dict,gdf: gpd.GeoDataFrame,target_field: str):
+        """Executes the calculation strategy."""
         pass        
 
 class SpatialStrategy(CalcStrategy):
@@ -70,7 +67,7 @@ class LogicStrategy(CalcStrategy):
             for rule in rules:
                 condition_str = rule.get("if")
                 result_val = rule.get("then")
-                mask = gdf.eval(condition_str) 
+                mask = gdf.eval(condition_str)
                 conditions.append(mask)
                 choices.append(result_val)
             gdf[target_field] = np.select(conditions, choices, default=default_val)
@@ -97,6 +94,132 @@ class VectorStrategy(CalcStrategy):
             raise ValueError("Division by zero detected in expression.")
         except Exception as e:
             raise ValueError(f"VectorStrategy failed to execute expression '{expression}': {e}")  
+class ExpressionParser:
+    """
+    Utility class to route expressions to the appropriate calculation strategy.
+    """
+    @staticmethod
+    def get_strategy(payload: dict):
+        """
+        Analyzes the payload to determine the best execution engine.
+        """
+        try:
+            strategy_type = payload.get("strategy", "").lower()
+            if strategy_type == "spatial":
+                return SpatialStrategy()
+            elif strategy_type == "logic":
+                return LogicStrategy()
+            elif strategy_type == "vector":
+                return VectorStrategy()
+        except Exception as e:
+            raise ValueError(f"ExpressionParser failed to determine strategy for expression '{strategy_type}': {e}")
+
+class FieldCalculator:
+    """
+    Facade class that provides a high-level API for calculating field values.
+    """
+    def __init__(self,payload,target_field,url):
+        """
+        Initializes the calculator.
+
+        Args:
+            gdf (gpd.GeoDataFrame): Data to process.
+            expression (str): Expression to evaluate (e.g., "!area! / 1000").
+            target_field (str): Destination field name.
+        """
+        self.payload=payload
+        self.target_field=target_field
+        self.url=url
+        self.db_port=os.getenv('DB_PORT')
+        self.db_host=os.getenv('DB_HOST')
+        self.db_name=os.getenv('DB_NAME')
+        self.db_user=os.getenv('DB_USER')
+        self.db_password=os.getenv('DB_PASSWORD')
+        
+    def calculate(self,layer_name):
+        """
+        Parses the expression and executes the strategy.
+        
+        Returns:
+            gpd.GeoDataFrame: Data with the new/updated field.
+            
+        Raises:
+            ValueError: If the input GeoDataFrame is invalid.
+        """
+        try:
+            gdf=gpd.read_file(self.url)
+            strategy=ExpressionParser.get_strategy(self.payload)
+            gdf=strategy.execute(payload=self.payload,gdf=gdf,target_field=self.target_field)
+            engine=create_engine(f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}")
+            gdf.to_postgis(layer_name,engine,if_exists='replace')
+         
+        except Exception as e:
+            raise ValueError(f"FieldCalculator failed to calculate field '{self.target_field}': {e}")
+
+class GeoserverCalcField:
+    def __init__(self):
+        """Initializes the Geoserver client.
+
+        The constructor loads credentials and the GeoServer URL from environment
+        variables for authentication and connectivity.
+        """
+        self.username = os.getenv('GEOSERVER_USERNAME')
+        self.password = os.getenv('GEOSERVER_PASSWORD')
+        self.geoserver_url = os.getenv('GEOSERVER_URL')
+        self.db_port=os.getenv('DB_PORT')
+        self.db_host=os.getenv('DB_HOST')
+        self.db_name=os.getenv('DB_NAME')
+        self.db_user=os.getenv('DB_USER')
+        self.db_password=os.getenv('DB_PASSWORD')
+    def get_vector_layer(self, workspace, layername):
+        """Retrieves the WFS URL for a vector layer.
+
+        :param workspace: The name of the GeoServer workspace containing the layer.
+        :type workspace: str
+        :param layername: The name of the vector layer.
+        :type layername: str
+        :returns: The WFS URL for the specified layer, formatted as JSON.
+        :rtype: str
+        """
+        url = (f"{self.geoserver_url}/{workspace}/ows?service=WFS&version=1.0.0&request=GetFeature"
+               f"&typeName={workspace}:{layername}&outputFormat=application/json")
+        return url
+    def get_store_name(self,workspace, layer_name):
+        info_url = f"{self.geoserver_url}/rest/workspaces/{workspace}/featuretypes/{layer_name}.json"
+        auth = (self.username, self.password)
+        try:
+            response = requests.get(info_url, auth=auth)
+            if response.status_code == 200:
+                data = response.json()
+                store_name = data['featureType']['store']['name']
+                store_name=store_name.split(':')[1]
+            return store_name   
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    def update_datastore(self,workspace,layername):
+        """Updates the datastore in GeoServer to reflect changes in the database.
+
+        :param workspace: The name of the GeoServer workspace.
+        :type workspace: str
+        :param layername: The name of the layer associated with the datastore.
+        :type layername: str
+        """
+        data_store=self.get_store_name(workspace, layername)
+        url = f"{self.geoserver_url}/rest/workspaces/{workspace}/datastores/{data_store}/featuretypes/{layername}/reset"
+        try:
+            response = requests.post(
+                url,
+                auth=(self.username, self.password),
+                headers={"Accept": "application/json"} 
+            )
+            if response.status_code == 200:
+                print(f"Successfully reset cache for: {layername}")
+            else:
+                print(f"Failed to reset. Status code: {response.status_code}")
+                print(f"Response: {response.text}")
+        except Exception as e:
+            raise ValueError(f"An error occurred: {e}")
+                    
         
 # test expressions 
 # expression={
@@ -117,3 +240,9 @@ class VectorStrategy(CalcStrategy):
 #     { "method": "buffer", "params": { "distance": 0.5 } },
 #     { "method": "centroid" }
 #   ]}        
+# test run
+# geoserver=GeoserverCalcField()
+# layer_url=geoserver.get_vector_layer(workspace="topp",layername="states")
+# calculator=FieldCalculator(payload=expression,target_field="newfield",url=layer_url)
+# calculator.calculate(layer_name="states")
+# geoserver.update_datastore(workspace="topp",layername="states")
