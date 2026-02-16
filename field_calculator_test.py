5ff07f7124c9d76d25313faf5e71c10aa1b1ffbd
yousefeleraky11 <yr.eraqy@wakeb.tech>
2026-02-16 09:03:54 +0200

    added endpoint for validation

--- field_calculator_test.py
+++ field_calculator_test.py
@@ -2,7 +2,7 @@ import re
 import geopandas as gpd
 import pandas as pd
 from abc import ABC, abstractmethod
-from pandas.api.types import is_numeric_dtype
+from pandas.api.types import is_numeric_dtype, is_string_dtype,is_object_dtype
 import numpy as np
 import os
 import requests
@@ -16,7 +16,7 @@ class CalcStrategy(ABC):
     @abstractmethod
     def execute(self, payload: dict,gdf: gpd.GeoDataFrame,target_field: str):
         """Executes the calculation strategy."""
-        pass        
+        pass
 
 class SpatialStrategy(CalcStrategy):
     """
@@ -24,7 +24,7 @@ class SpatialStrategy(CalcStrategy):
     Supports: Properties, Methods, Static Args, and Dynamic Column Args.
     """
     def execute(self, payload: dict, gdf: gpd.GeoDataFrame, target_field: str):
-        result = gdf.geometry
+        geom = gdf.geometry
         for operation in payload.get('operations', []):
             method_name = operation['method']
             args = operation.get('args', [])
@@ -38,14 +38,14 @@ class SpatialStrategy(CalcStrategy):
                     resolved_args.append(gdf[col_name])
                 else:
                     resolved_args.append(arg)
-            if not hasattr(result, method_name):
+            if not hasattr(geom, method_name):
                 raise ValueError(f"Geometry object has no method/property '{method_name}'")
-            attr = getattr(result, method_name)
+            attr = getattr(geom, method_name)
             if callable(attr):
-                result = attr(*resolved_args, **kwargs)
+                geom  = attr(*resolved_args, **kwargs)
             else:
-                result = attr
-        gdf[target_field] = result
+                geom = attr
+        gdf[target_field] = geom
         # if isinstance(result, (gpd.GeoSeries, gpd.GeoDataFrame)):
         #      gdf.set_geometry(target_field, inplace=True)
         #      gdf.drop(columns=['geometry'], inplace=True)
@@ -196,10 +196,6 @@ class FieldCalculator:
         """
         try:
             gdf=gpd.read_file(self.url)
-            for col in gdf.columns:
-                if " " in col:
-                    new_col = col.replace(" ", "_")
-                    gdf.rename(columns={col: new_col}, inplace=True)
             strategy=ExpressionParser.get_strategy(self.payload)
             gdf=strategy.execute(payload=self.payload,gdf=gdf,target_field=self.target_field)
             engine=create_engine(f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}")
@@ -209,13 +205,84 @@ class FieldCalculator:
         except Exception as e:
             raise ValueError(f"FieldCalculator failed to calculate field '{self.target_field}': {e}")
 
+class ExpressionValidator:
+    """
+    JOB: Validate payload logic against layer schema without loading data.
+    """
+    def __init__(self, url, payload):
+        self.url = url
+        self.payload = payload
+        try:
+            self.gdf_schema=gpd.read_file(self.url,rows=1)
+        except Exception as e:
+            raise ValueError(f"Could not read layer schema from {url}: {e}")
+    def _check_columns(self,columns):
+        missing_columns=[col for col in columns if col not in self.gdf_schema.columns]
+        if missing_columns:
+            raise ValueError(f"Missing columns: {', '.join(missing_columns)}")
+    def _validate_vector(self):
+        try:
+            expression = self.payload.get("expression", "")
+            fields = re.findall(r'!(.*?)!', expression)
+            self._check_columns(fields)
+            for field in fields:
+                if not is_numeric_dtype(self.gdf_schema[field]):
+                    raise ValueError(f"Field {field} is not numeric.")
+        except Exception as e:
+            raise ValueError(f"Expression validation failed: {e}")        
+    def _validate_logic(self):
+        try:
+            rules = self.payload.get("rules", [])
+            for rule in rules:
+                condition_str = rule.get("if")
+                fields = re.findall(r'!(.*?)!', condition_str)
+                self._check_columns(fields)
+        except Exception as e:
+            raise ValueError(f"Logic validation failed: {e}")
+    def _validate_string(self):
+        try:
+            col_a=self.payload.get("col_A")
+            col_b=self.payload.get("col_B")
+            if col_a not in self.gdf_schema.columns and col_b not in self.gdf_schema.columns:
+                raise ValueError(f"Missing columns: {col_a}, {col_b}")
+            if not is_object_dtype(self.gdf_schema[col_a]) and not is_object_dtype(self.gdf_schema[col_b]):   
+                raise ValueError(f"Columns {col_a} and {col_b} are not string.")
+        except Exception as e:
+            raise ValueError(f"String validation failed: {e}") 
+    def _validate_date(self):
+        try:
+            date_col=self.payload.get("date_col")
+            self._check_columns([date_col])
+            pd.to_datetime(self.gdf_schema[date_col])
+            operation=self.payload.get("operation")
+            if operation =="to_date":
+                date=self.payload.get("date")
+                pd.to_datetime(date)
+        except Exception as e:
+            raise ValueError(f"Date validation failed for column {date_col}: {e}")    
+    def validate(self):
+        try:
+            strategy=self.payload.get("strategy")
+            if strategy=="vector":
+                self._validate_vector()
+            elif strategy=="logic":
+                self._validate_logic()
+            elif strategy=="string":
+                self._validate_string()
+            elif strategy=="date":
+                self._validate_date()
+            print(f"the {strategy} strategy is valid for the given payload and layer schema.")    
+            return {"status":"success","message":"Expression is valid."}    
+        except Exception as e:
+            print(f"Validation error: {e}")
+            raise ValueError(f"Validation failed: {e}")            
+
 class GeoserverCalcField:
     """
     Facade class for calculating field values using GeoServer.
     """
     def __init__(self):
         """Initializes the Geoserver client.
-
         The constructor loads credentials and the GeoServer URL from environment
         variables for authentication and connectivity.
         """
@@ -229,7 +296,6 @@ class GeoserverCalcField:
         self.db_password=os.getenv('DB_PASSWORD')
     def get_vector_layer(self, workspace, layername):
         """Retrieves the WFS URL for a vector layer.
-
         :param workspace: The name of the GeoServer workspace containing the layer.
         :type workspace: str
         :param layername: The name of the vector layer.
@@ -242,7 +308,6 @@ class GeoserverCalcField:
         return url
     def get_store_name(self,workspace, layer_name):
         """Retrieves the datastore name for a given layer in GeoServer.
-
         :param workspace: The name of the GeoServer workspace.
         :type workspace: str
         :param layer_name: The name of the layer associated with the datastore.
@@ -264,7 +329,6 @@ class GeoserverCalcField:
             print(f"An error occurred: {e}")
     def update_datastore(self,workspace,layername):
         """Updates the datastore in GeoServer to reflect changes in the database.
-
         :param workspace: The name of the GeoServer workspace.
         :type workspace: str
         :param layername: The name of the layer associated with the datastore.
@@ -287,41 +351,39 @@ class GeoserverCalcField:
                 print(f"Response: {response.text}")
         except Exception as e:
             raise ValueError(f"An error occurred: {e}") from e
-                    
-        
-
-expression={
-    "strategy":"logic"
-    ,"rules":[
-        {"if":"Accident_Number<0","then": "safe"},
-        
-        ],"else":"false12"
-}
 # expression={
-#     "strategy":"date",
-#     "operation":"to_date",
-#     "date_col":"Date_and_Time",
-#     "date":"2018-01-01"
+#     "strategy":"logic"
+#     ,"rules":[
+#         {"if":"!AccidentNumber!<0","then": "safe"},
+        
+#         ],"else":"false12"
 # }
 expression={
+    "strategy":"date",
+    "operation":"to_date",
+    "date_col":"Date_and_Time",
+    "date":"2018-01-01"
+}
+expression={
   "strategy": "vector",
-  "expression": "Accident_Number * 24"
+  "expression": "!Accident_Number! * 0"
 }
 expression={
   "strategy": "spatial",
   "operations": [
-    {"method": "buffer", "kwargs": { "distance": 0.5  }}]}
-# expression={
-#   "strategy": "string",
-#   "method": "concat",
-#   "col_A": "Weather_Description",
-#   "col_B": "1234",
-#   "separator": " - "
-# }
-# #   ]}        
+    {"method": "buffer", "kwargs": { "distance": 0.5  }},
+    {"method": "centroid"}]}
+expression={
+  "strategy": "string",
+  "method": "concat",
+  "col_A": "Weather_Description",
+  "col_B": "1234",
+  "separator": " - "
+}
+# #   ]}
 # # test run
-geoserver=GeoserverCalcField()
-layer_url=geoserver.get_vector_layer(workspace="field_calc",layername="hello_test1")
-calculator=FieldCalculator(payload=expression,target_field="newfield1",url=layer_url)
-calculator.calculate(layer_name="hello_test1")
-geoserver.update_datastore(workspace="field_calc",layername="hello_test1")
+# geoserver=GeoserverCalcField()
+# layer_url=geoserver.get_vector_layer(workspace="field_calc",layername="hello_test1")
+# calculator=FieldCalculator(payload=expression,target_field="newfield1",url=layer_url)
+# calculator.calculate(layer_name="hello_test1")
+# geoserver.update_datastore(workspace="field_calc",layername="hello_test1")
--- src/main.py
+++ src/main.py
@@ -2,46 +2,47 @@ from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 import uvicorn
 import geopandas as gpd
-from  field_calculator import FieldCalculator,GeoserverCalcField
+# from  field_calculator import FieldCalculator,GeoserverCalcField
+from field_calculator_test import ExpressionValidator,GeoserverCalcField
 app = FastAPI()
 class CalcFieldsInputs(BaseModel):
-    """Input model for the calculate_fields endpoint."""
+    """Input model for field calculation."""
     workspace: str
     layername: str
-    store_name: str
-    expression: str
+    payload: dict
     target_field: str
-@app.post("/field_calculator")
-async def calculate_fields(inputs:CalcFieldsInputs):
-    """Calculates and updates fields in a GeoServer layer based on an expression.
-    Args:
-        inputs (CalcFieldsInputs): Input parameters including workspace, layername,
-                                    store_name, expression, and target_field.
-    Returns:
-        dict: Metadata about the updated layer including name, type, geometry type,
-              bounding box, and area.
-    """
-    try:    
-        
-        geoserver =GeoserverCalcField()
-        layer_url=geoserver.get_vector_layer(workspace=inputs.workspace,
-                                             layername=inputs.layername)
-        gdf =gpd.read_file(layer_url)
-        calculator=FieldCalculator(gdf=gdf,expression=inputs.expression,
-                                   target_field=inputs.target_field)
-        calculator.calculate(layer_name=inputs.layername)
-        geoserver.update_datastore(workspace=inputs.workspace,
-                                                      datastore=inputs.store_name,
-                                                      layername=inputs.layername)
-        new_layer={"layer_name": inputs.layername,
-            "layer_type": "vector",
-            "geometry_type": gdf.geom_type.unique().tolist(),
-            "bbox": gdf.total_bounds.tolist(),
-            "area": gdf.geometry.area.sum(),
-            }
-        return new_layer
+# @app.post("/field_calculator")
+# async def calculate_fields(inputs:CalcFieldsInputs):
+#     try:
+#         geoserver =GeoserverCalcField()
+#         layer_url=geoserver.get_vector_layer(workspace=inputs.workspace,
+#                                              layername=inputs.layername)
+#         gdf =gpd.read_file(layer_url)
+#         calculator=FieldCalculator(gdf=gdf,expression=inputs.expression,
+#                                    target_field=inputs.target_field)
+#         calculator.calculate(layer_name=inputs.layername)
+#         geoserver.update_datastore(workspace=inputs.workspace,
+#                                                       layername=inputs.layername)
+#         new_layer={"layer_name": inputs.layername,
+#             "layer_type": "vector",
+#             "geometry_type": gdf.geom_type.unique().tolist(),
+#             "bbox": gdf.total_bounds.tolist(),
+#             "area": gdf.geometry.area.sum(),
+#             }
+#         return new_layer
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=str(e))
+@app.post('/expression-validator')
+def validate_expression(inputs:CalcFieldsInputs):
+    """Endpoint to validate the expression for field calculation."""
+    try:
+        geoserver=GeoserverCalcField()
+        geoserver_url=geoserver.get_vector_layer(workspace=inputs.workspace,
+                                                 layername=inputs.layername)
+        validator=ExpressionValidator(url=geoserver_url,payload=inputs.payload)
+        return validator.validate()
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9091)
     
